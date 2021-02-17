ddcab691c09b1a7c8866f40d4b9b667ee895c72d
Luciano <majorass206@gmail.com>
2021-02-17 09:18:32 +0800

    setting mongo

---
+++ index.js
@@ -0,0 +1,35 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const typeDefs = require('./graphql/typeDefs');
+const Post = require('./models/Post');
+const { MONGODB } = require('./config.js');
+
+
+const resolvers = {
+    Query: {
+        async getPosts(){
+            try{
+                const posts = await Post.find();
+                return posts;
+            } catch(err){
+                throw new Error(err);
+            }
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+mongoose.connect(MONGODB, { useNewUrlParser: true })
+    .then(() => {
+        console.log('MongoDB Connected');
+        return server.listen({ port: 5000 });
+    })
+    .then(res => {
+        console.log(`Server running at ${res.url}`);
+    });
