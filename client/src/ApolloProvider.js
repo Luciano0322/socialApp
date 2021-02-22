5e6d86cc16bd694271ee48795b24757018c0f0d1
Luciano <majorass206@gmail.com>
2021-02-22 16:57:05 +0800

    Adding posts

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -4,14 +4,24 @@ import { ApolloClient } from '@apollo/client';
 import { InMemoryCache } from '@apollo/client/cache';
 import { createHttpLink } from '@apollo/client/link/http';
 import { ApolloProvider } from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
     //接應外層
     uri: 'http://localhost:5000'
 });
 
+const authLink = setContext(() => {
+    const token = localStorage.getItem('jwtToken');
+    return {
+        headers: {
+            Authorization: token ? `Bearer ${token}` : ''
+        }
+    };
+});
+
 const client = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 });
 
---
+++ client/src/components/PostForm.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Button, Form } from 'semantic-ui-react';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+
+import { useForm } from '../util/hook';
+import { FETCH_POSTS_QUERY } from '../util/graphql';
+
+function PostForm() {
+
+    const { values, onChange, onSubmit } = useForm(createPostCallback, {
+        body: ''
+    });
+
+    const [ createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
+        variables: values,
+        update(proxy, result){
+            const data = proxy.readQuery({
+                query: FETCH_POSTS_QUERY
+            });
+            // 在apollo V3 環境下直接更改緩存通常會導致錯誤行為，因此在v3中，緩存現在是只讀的。 
+            // 如果需要更改緩存中的數據，則現在需要在變異之前製作數據的副本
+            proxy.writeQuery({
+                query: FETCH_POSTS_QUERY,
+                data: {
+                    getPosts: [result.data.createPost, ...data.getPosts],
+                }
+            });
+            values.body = '';
+        }
+    });
+
+    function createPostCallback() {
+        createPost();
+    }
+
+    return (
+        <Form onSubmit={onSubmit}>
+            <h2>Create a post:</h2>
+            <Form.Field>
+                <Form.Input
+                    placeholder="Hi world!"
+                    name="body"
+                    onChange={onChange}
+                    value={values.body}
+                />
+                <Button type="submit" color="teal">
+                    Submit
+                </Button>
+            </Form.Field>
+        </Form>
+    )
+}
+
+const CREATE_POST_MUTATION = gql`
+    mutation createPost($body: String!){
+        createPost(body: $body){
+            id
+            body
+            createdAt
+            username
+            likes{
+                id
+                username
+                createdAt
+            }
+            likeCount
+            comments{
+                id
+                body
+                username
+                createdAt
+            }
+            commentCount
+        }
+    }
+`
+
+export default PostForm;
